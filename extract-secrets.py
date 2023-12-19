91d40b69b8bd979639d3c5ac59071d2cea2c43b4
phonezawphyo <phonezawphyo@gmail.com>
2023-12-19 16:23:46 +0900

    Create extract-secrets.py

---
+++ extract-secrets.py
@@ -0,0 +1,12 @@
+import json,sys;
+
+if __name__ == '__main__':
+  payload = json.loads(''.join(sys.stdin.readlines()))
+  secrets = json.loads(payload['SecretString'])
+  print(
+    '\n'.join(
+      [
+        f'{x}={v}' for (x,v) in secrets.items()
+      ]
+    )
+  )
