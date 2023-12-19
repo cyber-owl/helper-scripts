ddf77a1928afab95640ef488ec0972fa238090e8
phonezawphyo <phonezawphyo@gmail.com>
2023-12-19 16:18:55 +0900

    Update and rename aliases-secrets.sh to create-extract-secrets-py.sh

--- create-extract-secrets-py.sh
+++ create-extract-secrets-py.sh
@@ -1,4 +1,4 @@
-printf -v OWL_HELPERS_EXTRACT_SECRETS_PY "%q" "import json,sys;
+echo "import json,sys;
 
 if __name__ == '__main__':
   payload = json.loads(''.join(sys.stdin.readlines()))
@@ -9,6 +9,4 @@ if __name__ == '__main__':
         f'{x}={v}' for (x,v) in secrets.items()
       ]
     )
-  )"
-
-alias  owl-helpers-create-extract-secrets-py="echo $OWL_HELPERS_EXTRACT_SECRETS_PY > extract-secrets.py"
+  )" > extract-secrets.py
