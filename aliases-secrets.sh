dfc44cbb49be34bbb45675501d19f2af65084b39
phonezawphyo <phonezawphyo@gmail.com>
2023-12-19 16:14:24 +0900

    Update aliases-secrets.sh

--- aliases-secrets.sh
+++ aliases-secrets.sh
@@ -1,14 +1,14 @@
-OWL_HELPERS_EXTRACT_SECRETS_PY="import json,sys;
+printf -v OWL_HELPERS_EXTRACT_SECRETS_PY "%q" "import json,sys;
 
 if __name__ == '__main__':
   payload = json.loads(''.join(sys.stdin.readlines()))
   secrets = json.loads(payload['SecretString'])
   print(
-    '\\n'.join(
+    '\\\n'.join(
       [
         f'{x}={v}' for (x,v) in secrets.items()
       ]
     )
   )"
 
-alias owl-helpers-create-extract-secrets-py="echo \"${EXTRACT_SECRETS_PY}\" > extract-secrets.py"
+alias  owl-helpers-create-extract-secrets-py="echo $OWL_HELPERS_EXTRACT_SECRETS_PY > extract-secrets.py"
