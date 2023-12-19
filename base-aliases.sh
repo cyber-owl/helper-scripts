ef7f56472e9d7079ff973dc21fb733d4a932dd85
phonezawphyo <phonezawphyo@gmail.com>
2023-12-19 15:51:13 +0900

    Create base-aliases.sh

---
+++ base-aliases.sh
@@ -0,0 +1,14 @@
+EXTRACT_SECRETS_PY="import json,sys;
+
+if __name__ == '__main__':
+  payload = json.loads(''.join(sys.stdin.readlines()))
+  secrets = json.loads(payload['SecretString'])
+  print(
+    '\n'.join(
+      [
+        f'{x}={v}' for (x,v) in secrets.items()
+      ]
+    )
+  )"
+
+alias create-extract-secrets-py="echo ${extract-secrets-py} > extract-secrets.py"
