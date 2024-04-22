b89ccbc1d9fb3e4f7eb5253a4bd805badb84fac7
phonezawphyo <phonezawphyo@gmail.com>
2024-04-22 15:11:52 +0900

    Update extract-secrets.py

--- extract-secrets.py
+++ extract-secrets.py
@@ -4,4 +4,4 @@ import sys
 if __name__ == '__main__':
     payload = json.loads(''.join(sys.stdin.readlines()))
     secrets = json.loads(payload['SecretString'])
-    print('\n'.join([f"{x}={v}" for (x, v) in secrets.items()]))
+    print("\n".join(["{}={}".format(x, v) for (x, v) in secrets.items()]))
