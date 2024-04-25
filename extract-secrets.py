414f87e1dad68d7f57a6f234df69c8cb9881c1ee
phonezawphyo <phonezawphyo@gmail.com>
2024-04-25 16:29:27 +0900

    Update extract-secrets.py

--- extract-secrets.py
+++ extract-secrets.py
@@ -2,6 +2,10 @@ import json
 import sys
 
 if __name__ == '__main__':
+    inline = sys.argv[1]
     payload = json.loads(''.join(sys.stdin.readlines()))
     secrets = json.loads(payload['SecretString'])
-    print("\n".join(["{}={}".format(x, (json.dumps(v) if '\n' in v else v)) for (x, v) in secrets.items()]))
+    if inline == 'inline':
+        print(" ".join(["{}={}".format(x, json.dumps(v)) for (x, v) in secrets.items()]))
+    else:
+        print("\n".join(["{}={}".format(x, (json.dumps(v) if '\n' in v else v)) for (x, v) in secrets.items()]))
